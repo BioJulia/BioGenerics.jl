12a01f5201463af888fca0cf9878ef5de83f840e
Ben J. Ward <benjward@protonmail.com>
2019-02-07 16:28:22 +0000

    Add testing module

--- src/BioGenerics.jl
+++ src/BioGenerics.jl
@@ -1,5 +1,20 @@
+# BioGenerics.jl
+# ==============
+#
+# Core types and methods common to many packages in the BioJulia ecosystem.
+#
+# This file is a part of BioJulia.
+# License is MIT: https://github.com/BioJulia/BioCore.jl/blob/master/LICENSE.md
+
 module BioGenerics
 
-greet() = print("Hello World!")
+#include("declare.jl")
+#include("Exceptions.jl")
+#include("IO.jl")
+#include("Mem.jl")
+#include("Ragel.jl")
+#include("ReaderHelper.jl")
+#include("RecordHelper.jl")
+include("Testing.jl")
 
-end # module
+end # module BioGenerics
