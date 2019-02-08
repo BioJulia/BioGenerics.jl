d13ca556c0c9711b2c80971ecb68e2029c2d4f6e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-08 01:30:14 +0000

    build based on 573de2c

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#BioGenerics-1",
+    "page": "Home",
+    "title": "BioGenerics",
+    "category": "section",
+    "text": "Modules = [BioGenerics]"
+},
+
+]}
